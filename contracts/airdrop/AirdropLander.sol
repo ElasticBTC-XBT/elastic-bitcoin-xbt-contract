b8671625ece08f395b4711d07b4736f9d17f71ae
Alan The Achiever <alantheachiever@elasticbitcoin.org>
2020-12-27 00:38:00 +0700

    fix: invalid opcode in airdrop lander

--- contracts/airdrop/AirdropLander.sol
+++ contracts/airdrop/AirdropLander.sol
@@ -1,24 +1,28 @@
 pragma solidity >=0.6.8;
 
+import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 import "../lib/ERC20.sol";
 
 contract AirdropLander {
+    using SafeMath for uint256;
     IERC20 public tokenInstance;
     uint256 public claimableAmount;
 
     mapping(address => bool) participants;
 
-    constructor(address _tokenInstance, uint256 claimableAmount) public {
-        require(_tokenInstance != address(0));
+    constructor(address _tokenInstance, uint256 _claimableAmount) public {
+        require(_tokenInstance != address(0), 'Error: cannot add token at NoWhere :)');
+        require(uint256(_claimableAmount) > 0, 'Error: claimable amount cannot be zero');
+
         tokenInstance = IERC20(_tokenInstance);
-        claimableAmount = claimableAmount;
+        claimableAmount = uint256(_claimableAmount);
     }
 
     function requestTokens() public {
-        uint256 contractXBTFund = tokenInstance.balanceOf(address(this));
+        uint256 contractXBTFundRatio = tokenInstance.balanceOf(address(this)).div(claimableAmount);
 
         require(
-            (contractXBTFund / claimableAmount) > 1,
+            contractXBTFundRatio > 1,
             'Error: contract fund is exceeded'
         );
 
