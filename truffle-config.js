90bd5217b5b263a807af249406aec6f5c8e330ca
Alan The Achiever <alantheachiever@elasticbitcoin.org>
2020-12-26 21:39:41 +0700

    build: added truffle build command

--- truffle-config.js
+++ truffle-config.js
@@ -41,18 +41,18 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/07b667eff25947b7bb9fce9bde73efda")
       },
       network_id: 3
-    }
+    },
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    ganache: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
+     network_id: 5777,       // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
