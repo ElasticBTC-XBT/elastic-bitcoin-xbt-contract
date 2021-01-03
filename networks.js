8a2098daa07d0b534c80ff70d5300d26d1c2a3d9
do1thingwell <do1thingwell@lsdventures.com>
2021-01-03 13:04:17 +0700

    alchemy

--- networks.js
+++ networks.js
@@ -6,6 +6,18 @@ module.exports = {
       url: 'http://127.0.0.1:7545',
       chainId: 5777
     },
+
+    hardhat_local: {
+      url: 'http://127.0.0.1:8545',
+    },
+
+    
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_ID}`
+        //,      blockNumber: 11095000
+        }
+    },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`,
       chainId: 4,
