61ae0c4e27bf254322db96008f259e2c156f3151
Tran Dinh Khang <khangtd.09@gmail.com>
2021-04-14 22:20:45 +0700

    feat: bump nodejs version

--- truffle-config.js
+++ truffle-config.js
@@ -27,14 +27,14 @@ const web3 = require('web3');
 
 module.exports = {
   /**
-     * Networks define how you connect to your ethereum client and let you set the
-     * defaults web3 uses to send transactions. If you don't specify one truffle
-     * will spin up a development blockchain for you on port 9545 when you
-     * run `develop` or `test`. You can ask a truffle command to use a specific
-     * network from the command line, e.g
-     *
-     * $ truffle test --network <network-name>
-     */
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
 
   networks: {
     mainnet: {
