8040d7848db5142ad4235071853d4729b3ee4af7
Tam Nguyen <nntam2013@gmail.com>
2021-05-04 17:10:38 +0700

    chore: revert migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,13 +1,5 @@
-// const Migrations = artifacts.require("XBN");
+const Migrations = artifacts.require("Migrations");
 
-// module.exports = function(deployer) {
-//   deployer.deploy(Migrations);
-// };
-
-// const XBN = artifacts.require("XBN");
-
-// const { deployProxy } = require("@openzeppelin/truffle-upgrades");
-
-// module.exports = async function(deployer) {
-//   await deployProxy(XBN, [42], { deployer, initializer: "store" });
-// };
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+};
