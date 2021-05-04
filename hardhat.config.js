1fb9b54015ebe1cfcc769fdb0588c09a0d9ab5dd
Tam Nguyen <nntam2013@gmail.com>
2021-05-04 17:07:29 +0700

    WIP

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,25 +1,26 @@
-require('dotenv').config();
-require('@nomiclabs/hardhat-waffle');
-require('@nomiclabs/hardhat-web3');
-require('@nomiclabs/hardhat-ethers');
-require('@openzeppelin/hardhat-upgrades');
-require('solidity-coverage');
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("solidity-coverage");
+require("@nomiclabs/hardhat-etherscan");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-async function deployXBT () {
+async function deployXBT() {
   // We get the contract to deploy
   const accounts = await web3.eth.getAccounts();
-  const xbtContract = await ethers.getContractFactory('XBT');
+  const xbtContract = await ethers.getContractFactory("XBN");
   const xbt = await upgrades.deployProxy(xbtContract, [accounts[0]]);
 
-  console.log('XBT deployed to:', xbt.address);
-  console.log('XBT deployed by:', accounts[0]);
+  console.log("XBT deployed to:", xbt.address);
+  console.log("XBT deployed by:", accounts[0]);
 }
 
-task('deployXBT', 'Deploy XBT Contract').setAction(async () => {
+task("deployXBT", "Deploy XBT Contract").setAction(async () => {
   await deployXBT();
 });
 
@@ -37,7 +38,7 @@ task('deployXBT', 'Deploy XBT Contract').setAction(async () => {
 //     await deployXBTRebaser();
 // });
 
-async function deployAirdropLander (
+async function deployAirdropLander(
   dTokenAddress,
   claimableAmount,
   bonusPeriodSecs,
@@ -45,7 +46,9 @@ async function deployAirdropLander (
   maxBonusRatio
 ) {
   // We get the contract to deploy
-  const airdropLanderContract = await ethers.getContractFactory('AirdropLander');
+  const airdropLanderContract = await ethers.getContractFactory(
+    "AirdropLander"
+  );
 
   const lander = await airdropLanderContract.deploy(
     dTokenAddress,
@@ -55,16 +58,16 @@ async function deployAirdropLander (
     maxBonusRatio
   );
 
-  console.log('AirdropLander deployed to:', lander.address);
+  console.log("AirdropLander deployed to:", lander.address);
 }
 
-task('deployAirdropLander', 'Deploy AirdropLander')
-  .addParam('address', "The distribution token's address")
-  .addParam('claimable', 'Claimable amount')
-  .addParam('bonusperiodsecs', 'Duration time between 2 claims')
-  .addParam('minbonusratio', 'Min bonus ratio')
-  .addParam('maxbonusratio', 'Max bonuns ratio')
-  .setAction(async taskArgs => {
+task("deployAirdropLander", "Deploy AirdropLander")
+  .addParam("address", "The distribution token's address")
+  .addParam("claimable", "Claimable amount")
+  .addParam("bonusperiodsecs", "Duration time between 2 claims")
+  .addParam("minbonusratio", "Min bonus ratio")
+  .addParam("maxbonusratio", "Max bonuns ratio")
+  .setAction(async (taskArgs) => {
     await deployAirdropLander(
       taskArgs.address,
       taskArgs.claimable,
@@ -75,7 +78,16 @@ task('deployAirdropLander', 'Deploy AirdropLander')
   });
 
 module.exports = {
-  solidity: '0.6.8',
-  defaultNetwork: 'ganache',
-  networks: require('./networks').networks
+  solidity: "0.8.2",
+  defaultNetwork: "ganache",
+  networks: require("./networks").networks,
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  etherscan: {
+    apiKey: "1CP1YJFMPHJD54Z88EYDHFRU6KN82Z4DRR",
+  },
 };
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
-const Migrations = artifacts.require("Migrations");
+// const Migrations = artifacts.require("XBN");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
-};
+// module.exports = function(deployer) {
+//   deployer.deploy(Migrations);
+// };
+
+// const XBN = artifacts.require("XBN");
+
+// const { deployProxy } = require("@openzeppelin/truffle-upgrades");
+
+// module.exports = async function(deployer) {
+//   await deployProxy(XBN, [42], { deployer, initializer: "store" });
+// };
