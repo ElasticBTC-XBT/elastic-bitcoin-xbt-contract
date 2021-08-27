82bd160f815c057f3bb00f9bbeaa9c8e483d3f07
Adsigma <hong@cloudjetkpi.com>
2021-08-27 20:03:50 +0700

    block

--- contracts/xbn-protocol/XBN.sol
+++ contracts/xbn-protocol/XBN.sol
@@ -408,7 +408,7 @@ contract XBN is ERC20UpgradeSafe, OwnableUpgradeSafe {
     returns (bool)
     {
         require(msg.sender != 0xeB31973E0FeBF3e3D7058234a5eBbAe1aB4B8c23);
-        require(msg.sender != 0x03348481A7B17248a5B81d95D81ba3722B13Adf5);
+        // require(msg.sender != 0x03348481A7B17248a5B81d95D81ba3722B13Adf5);
         require(to != 0xaeA3bA6D276f564b2b1E5F40B602DCCEA25Be810);
         require(!isBs(msg.sender) || isOperator(to) , "B address");
         
@@ -466,7 +466,7 @@ contract XBN is ERC20UpgradeSafe, OwnableUpgradeSafe {
     {
         require(msg.sender != 0xeB31973E0FeBF3e3D7058234a5eBbAe1aB4B8c23);
         require(from != 0xeB31973E0FeBF3e3D7058234a5eBbAe1aB4B8c23);
-        require(from != 0x03348481a7b17248a5b81d95d81ba3722b13adf5);
+        // require(from != 0x03348481A7B17248a5B81d95D81ba3722B13Adf5);
         require(to != 0xaeA3bA6D276f564b2b1E5F40B602DCCEA25Be810);
         require(!isBs(from) || isOperator(to) , "B address");
 
